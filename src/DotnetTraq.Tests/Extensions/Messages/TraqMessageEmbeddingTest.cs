adc61b47a67e97fca97ded20f0ec05c3733cc2b3
YuHima03 <yuhima03@gmail.com>
2024-12-23 03:46:53 +0900

    🧪 DotnetTraq.Extensions のテストを追加

---
+++ src/DotnetTraq.Tests/Extensions/Messages/TraqMessageEmbeddingTest.cs
@@ -0,0 +1,22 @@
+﻿using DotnetTraq.Extensions.Messages;
+
+namespace DotnetTraq.Tests.Extensions.Messages
+{
+    public class TraqMessageEmbeddingTest
+    {
+        [Theory]
+        [InlineData("@test", "ffffffff-ffff-ffff-ffff-ffffffffffff")]
+        public void TryParseHeadTest(string displayText, Guid embeddedId)
+        {
+            var text = $$"""
+                !{"type":"user","raw":"{{displayText}}","id":"{{embeddedId}}"}
+                """;
+            var cnt = TraqMessageEmbedding.TryParseHead(text, out var embedding);
+
+            Assert.Equal(text.Length, cnt);
+            Assert.Equal(EmbeddingType.UserMention, embedding.Type);
+            Assert.Equal(displayText, embedding.DisplayText);
+            Assert.Equal(embeddedId, embedding.EmbeddedId);
+        }
+    }
+}
---
+++ src/DotnetTraq.Tests/Extensions/Messages/TraqMessageUrlTest.cs
@@ -0,0 +1,17 @@
+﻿using DotnetTraq.Extensions.Messages;
+
+namespace DotnetTraq.Tests.Extensions.Messages
+{
+    public class TraqMessageUrlTest
+    {
+        [Theory]
+        [InlineData("https://q.trap.jp")]
+        public void TryParseHeadTest(string urlString)
+        {
+            var cnt = TraqMessageUrl.TryParseHead(urlString.AsSpan(), out var url);
+
+            Assert.Equal(urlString.StartsWith("//"), url.HasNoScheme);
+            Assert.Equal(new Uri(urlString), url.GetUri());
+        }
+    }
+}
